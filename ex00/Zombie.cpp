adcf60cb12d8ccf3b9622eb54f98d747bf7bd3d8
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2022-06-20 15:46:01 +0200

    Add zombie implementation

---
+++ ex00/Zombie.cpp
@@ -0,0 +1,25 @@
+#include "Zombie.hpp"
+
+#include <iostream>
+
+Zombie::Zombie() : _name("unnamed") { }
+
+Zombie::Zombie(const Zombie &other) : _name(other._name) { }
+
+Zombie::Zombie(const std::string &name) : _name(name) { }
+
+Zombie::~Zombie() {
+    std::cout << _name << " destroyed" << std::endl;
+}
+
+void Zombie::announce() const {
+    std::cout << _name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+Zombie *Zombie::newZombie(const std::string &name) {
+    return new Zombie(name);
+}
+
+void Zombie::randomChump(const std::string &name) {
+    Zombie(name).announce();
+}
