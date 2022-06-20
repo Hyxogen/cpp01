565e3dcf275753f16890a869220bfb37d1a75d27
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2022-06-20 15:54:39 +0200

    Add simple tests

---
+++ ex00/main.cpp
@@ -0,0 +1,12 @@
+#include "Zombie.hpp"
+
+int main() {
+	Zombie("Carl").announce();
+
+	Zombie* dynamic_zombie = newZombie("dynamic");
+	dynamic_zombie->announce();
+	randomChump("chump");
+
+	delete dynamic_zombie;
+	return (0);
+}
