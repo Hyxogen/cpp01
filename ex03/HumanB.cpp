4ad81485060410d408160053a1b9f5fdd734f623
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2022-06-21 07:26:47 +0200

    Add human headers and implementations

---
+++ ex03/HumanB.cpp
@@ -0,0 +1,17 @@
+#include "HumanB.hpp"
+
+#include <iostream>
+#include <cstddef>
+
+HumanB::HumanB(const std::string &name) : _name(name)  _weapon(NULL) { }
+
+void HumanB::setWeapon(Weapon &weapon) {
+	_weapon = &weapon;
+}
+
+void HumanB::attack() const {
+	if (_weapon == NULL) {
+		return;
+	}
+	std::cout << _name << " attacks with their " << _weapon.getType() << std::endl;
+}
